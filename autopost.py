a19bf80a8aa9b2f292a64a7d2571638bff3833e6
BongnamKim <vbnmzx1@skku.edu>
2020-08-10 21:11:25 +0900

    test1

--- autopost.py
+++ autopost.py
@@ -19,14 +19,13 @@ for i, name in enumerate(member):
     rank[i] = result[0]['tier']
     point[i] = result[0]['leaguePoints']
 
-GOOGLE_CHROME_PATH = os.environ.get('GOOGLE_CHROME_BIN')
-CHROMEDRIVER_PATH = os.environ.get("CHROMEDRIVER_PATH")
 chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = GOOGLE_CHROME_PATH
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--no-sandbox')
-driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
-driver = webdriver.Chrome(driver)
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
 driver.get('https://gall.dcinside.com/mgallery/board/lists?id=longzhugaming')
 driver.find_element_by_class_name('user_info').click()
 driver.find_element_by_id('id').send_keys(dcid)
