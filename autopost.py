ba9ccf71c21c788a5a853babe461a13db94cc207
BongnamKim <vbnmzx1@skku.edu>
2020-08-12 22:16:07 +0900

    head

--- autopost.py
+++ autopost.py
@@ -26,7 +26,6 @@ for i, name in enumerate(member):
 
 options = webdriver.ChromeOptions()
 options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-options.add_argument('--headless')
 options.add_argument('--remote-debugging-port=9222')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
