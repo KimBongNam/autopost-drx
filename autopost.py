70aed16f6e92c9e4ff0272b135ea54b4fede0d1e
BongnamKim <vbnmzx1@skku.edu>
2020-08-12 22:02:05 +0900

    test

--- autopost.py
+++ autopost.py
@@ -26,7 +26,6 @@ for i, name in enumerate(member):
 
 options = webdriver.ChromeOptions()
 options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-options.add_argument("--headless")
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--disable-gpu')
@@ -55,13 +54,13 @@ except:
 time.sleep(random.randint(1,5))
 driver.find_element_by_id('btn_write').click()
 driver.find_element_by_id('subject').send_keys('선수들 솔랭 점수.bot')
-driver.switch_to_frame(driver.find_element_by_xpath("//*[@id='tx_canvas_wysiwyg']"))
+driver.switch_to.frame(driver.find_element_by_xpath("//*[@id='tx_canvas_wysiwyg']"))
 
 for i in range(5):
     driver.find_element_by_tag_name("body").send_keys(f'{member[i]} {rank[i]} {point[i]}/{success[i]} \n')
 
-driver.switch_to_default_content()
+driver.switch_to.default_content()
 driver.implicitly_wait(random.randint(1,5))
 driver.find_element_by_xpath('//*[@id="write"]/div[5]/button[2]').click()
 
-
+print(driver.page_source)
