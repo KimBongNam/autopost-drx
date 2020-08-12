a7ce4485815e23dd031fa0de129cc6676fd39ee5
BongnamKim <vbnmzx1@skku.edu>
2020-08-12 22:08:18 +0900

    headless

--- autopost.py
+++ autopost.py
@@ -26,6 +26,8 @@ for i, name in enumerate(member):
 
 options = webdriver.ChromeOptions()
 options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+options.add_argument('--headless')
+options.add_argument('--remote-debugging-port=9222')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--disable-gpu')
